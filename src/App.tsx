4a49abcea68b500472d94e986f1a90d1fa7ce21d
rukshanperera12 <rukshan@itone.biz>
2025-11-05 11:03:08 +0530

    fix:added footer to ui

--- src/App.tsx
+++ src/App.tsx
@@ -2,19 +2,24 @@ import "./App.css";
 import Form from "./Components/Form";
 import Header from "./Components/Header";
 import List from "./Components/List";
+import Footer from "./Components/Footer";
 
 function App() {
   return (
-    <div className="container-fluid fs-5">
-      <Header />
+    <div className="container-fluid fs-5 d-flex flex-column min-vh-100">
+      <div className="flex-grow-1">
+        <Header />
 
-      <div className="d-flex justify-content-center align-items-center my-5">
-        <Form />
+        <div className="d-flex justify-content-center align-items-center my-5">
+          <Form />
+        </div>
+
+        <div className="d-flex justify-content-center align-items-center my-5">
+          <List />
+        </div>
       </div>
 
-      <div className="d-flex justify-content-center align-items-center my-5">
-        <List />
-      </div>
+      <Footer />
     </div>
   );
 }
---
+++ src/Components/Footer.tsx
@@ -0,0 +1,14 @@
+const Footer = () => {
+  return (
+    <div className="bg-info p-4 row">
+      <div className="col-6">
+        <h1 className="text-dark text-center">Total To DOs : 2</h1>
+      </div>
+      <div className="col-6">
+        <h1 className="text-dark text-center">Completed To DOs : 1</h1>
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
